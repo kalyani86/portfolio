5dc4490ba289c67f0a3c6e530cfdf4b198f0a657
kalyani86 <patil.kalyani8956@gmail.com>
2024-09-12 17:51:46 +0530

    ui component

--- src/components/ui/typewriter-effect.tsx
+++ src/components/ui/typewriter-effect.tsx
@@ -72,7 +72,7 @@ export const TypewriterEffect = ({
   return (
     <div
       className={cn(
-        "text-3xl sm:text-3xl md:text-3xl lg:text-5xl font-bold text-center",
+        "text-3xl sm:text-3xl md:text-4xl lg:text-5xl font-bold text-center",
         className
       )}
     >
